a1f0f76d030395d6c542165f76413ae5e0d47d91
sulisindriyani <sulisindriyani04@gmail.com>
2024-11-11 22:40:38 +0700

    Update C1_W4_Assignment.js

--- C1_Browser-based-TF-JS/W4/assignment/C1_W4_Assignment.js
+++ C1_Browser-based-TF-JS/W4/assignment/C1_W4_Assignment.js
@@ -25,20 +25,25 @@ async function train() {
   // using ReLu activation functions where applicable.
   model = tf.sequential({
     layers: [
-        
-      // YOUR CODE HERE
-
+	    tf.layers.flatten({inputShape: mobilenet.outputs[0].shape.slice(1)}), // Flatten layer
+	    tf.layers.dense({units: 128, activation: 'relu'}), // Hidden layer
+	    tf.layers.dense({units: 64, activation: 'relu'}), // Hidden layer
+	    tf.layers.dense({units: 5, activation: 'softmax'}) // Output layer
     ]
   });
     
    
   // Set the optimizer to be tf.train.adam() with a learning rate of 0.0001.
-  const optimizer = // YOUR CODE HERE
+  const optimizer = tf.train.adam(0.0001); // Optimizer dengan learning rate 0.0001
     
-        
+	  
   // Compile the model using the categoricalCrossentropy loss, and
   // the optimizer you defined above.
-  model.compile(// YOUR CODE HERE);
+  model.compile(
+	  optimizer: optimizer,
+	  loss: 'categoricalCrossentropy',
+	  metrics: ['accuracy']
+  });
  
   let loss = 0;
   model.fit(dataset.xs, dataset.ys, {
@@ -74,8 +79,10 @@ function handleButton(elem){
             
         // Add a case for lizard samples.
         // HINT: Look at the previous cases.
-            
-        // YOUR CODE HERE
+                case "4":
+			lizardSamples++;
+			document.getElementById("lizardsamples").innerText = "Lizard samples:" + lizardSamples;
+			break;
 		
             
 	}
@@ -111,8 +118,9 @@ async function predict() {
             
         // Add a case for lizard samples.
         // HINT: Look at the previous cases.
-            
-        // YOUR CODE HERE 
+                case 4:
+			predictionText = "I see Lizard";
+			break;
 	
             
 	}
